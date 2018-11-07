ebf8cdddd582458dc7aa07d2057092c12e1329ae
Mohamed <almashrgy@gmail.com>
2018-11-07 11:13:26 -0500

    add shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -9,6 +9,7 @@ export class ShoppingListComponent implements OnInit {
 ingredients: Ingredient[] = [
   new Ingredient('Apple',5),
   new Ingredient('Tomatoes',10),
+  new Ingredient('onion',4),
 ];
   constructor() { }
 
